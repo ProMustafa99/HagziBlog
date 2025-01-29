8d94d0c1e6342975456f81ec999b7bbb7ffa9469
ProMustafa99 <mustafaksalameh99@gmail.com>
2025-01-29 17:20:44 +0300

    Add interface for the FooterLinks Props

--- pages/_components/FooterLinks.tsx
+++ pages/_components/FooterLinks.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-export default function FooterLinks({tilte , children}) {
+
+interface FooterLink {
+  tilte : string,
+  children :ReactNode
+}
+export default function FooterLinks({tilte , children}:FooterLink) {
   return (
     <div className=''>
        <h4 className='text-customblue pt-5'> {tilte} </h4>
