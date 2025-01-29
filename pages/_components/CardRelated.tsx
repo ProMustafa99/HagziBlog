33e9996318512ab6be60222a673eda67d595f71b
ProMustafa99 <mustafaksalameh99@gmail.com>
2025-01-29 12:03:44 +0300

    Related articles - Rounded from top not from bottom

--- pages/_components/CardRelated.tsx
+++ pages/_components/CardRelated.tsx
@@ -2,25 +2,26 @@ import Image from "next/image";
 import ReadMoreButton from "./ReadMoreButton";
 
 export default function CardRelated({ related }: any) {
-  // Destructure with a fallback value for `related`
   const { id, title_en, cover_image_url } = related || {};
-
-  // Check if required properties are available
+ 
   if (!id || !title_en || !cover_image_url) {
-    return <div>Loading...</div>;  // Handle the case where data is missing
+    return <div>Loading...</div>;  
   }
 
   return (
-    <article className='mb-11 shadow-lg'>
+    <article className='mb-11 '>
       <div className='relative py-32'>
-        <Image src="/test.png" fill className='rounded-tl-lg rounded-tr-lg' alt={`Image/${title_en}`} />
+        <Image src="/test.png" fill className='rounded-t-lg ' alt={`Image/${title_en}`} />
       </div>
 
-      <div className='bg-white p-4'>
+      <div className='bg-white p-4 rounded-b-lg'>
+        
         <h3 className='text-oceandepth font-bold'> {title_en} </h3>
+        
         <div className='py-4'>
           <ReadMoreButton pathLink='/blog' id={id} />
         </div>
+
       </div>
     </article>
   );
